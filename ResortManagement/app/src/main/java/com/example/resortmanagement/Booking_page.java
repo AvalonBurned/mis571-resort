3d9d7cb62b05346d7065b6691ce1fbc909dab51a
Mia <34622179+mialotus@users.noreply.github.com>
2019-11-06 00:35:38 -0500

    Add files via upload

---
+++ ResortManagement/app/src/main/java/com/example/resortmanagement/Booking_page.java
@@ -0,0 +1,14 @@
+package com.example.resortmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Booking_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.booking_page);
+    }
+}
---
+++ ResortManagement/app/src/main/java/com/example/resortmanagement/login.java
@@ -0,0 +1,14 @@
+package com.example.resortmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+    }
+}
